db598cf4bcf3a7f2f95d0f078bf0b745dd95052f
Alexey Timofeev <lesha.timofeev2017@yandex.ru>
2023-03-23 11:02:01 +0300

    Initial commit

---
+++ src/main/java/ru/timofeev/projects/telegrambottranslator/TelegramBotTranslatorApplication.java
@@ -0,0 +1,13 @@
+package ru.timofeev.projects.telegrambottranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotTranslatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/timofeev/projects/telegrambottranslator/TelegramBotTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.timofeev.projects.telegrambottranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
